e964024233246c5df769baad8859db56eadf8889
Kristofer Walters <kris@wltrs.org>
2016-01-05 15:07:56 +0100

    Use webpack to build css instead of postcss-cli

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var postcssImport = require('postcss-import');
+var autoprefixer = require('autoprefixer');
+var cssNext = require('postcss-cssnext');
+var postcssReporter = require('postcss-reporter');
+
+module.exports = {
+  entry: './index.js',
+  target: 'web',
+
+  output: {
+    path: './dist',
+    filename: 'aout.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
+      }
+    ]
+  },
+
+  plugins: [ new ExtractTextPlugin('style.css') ],
+
+  postcss: function () {
+    return [
+      postcssImport({
+        glob: true,
+        addDependencyTo: this
+      }),
+      autoprefixer,
+      cssNext,
+      postcssReporter
+    ]
+  }
+};
