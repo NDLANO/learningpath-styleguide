35e84e4525875bd7cf522f9280b487d739f09afe
Kristofer Walters <kw@knowit.no>
2016-02-04 08:05:31 +0100

    Many structural changes to naming and file organisation
    A lot of BEM methodology here.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('./docs/index.jade');
 require('./docs/typography.jade');
 require('./docs/molecules.jade');
 require('./docs/pages.jade');
+require('./docs/pages-empty.jade');
+require('./docs/pages-title-empty.jade');
 require('./docs/pages-login.jade');
 require('./docs/pages-minestier.jade');
 
