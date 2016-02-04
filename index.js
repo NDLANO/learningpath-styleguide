e9ee126a5f58352984beedf19e00ec8d26011baf
Kristofer Walters <kw@knowit.no>
2016-02-04 10:08:54 +0100

    New empty page 'lærgingssti'

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ require('./docs/pages-empty.jade');
 require('./docs/pages-title-empty.jade');
 require('./docs/pages-login.jade');
 require('./docs/pages-minestier.jade');
+require('./docs/pages-laeringssti.jade');
 
